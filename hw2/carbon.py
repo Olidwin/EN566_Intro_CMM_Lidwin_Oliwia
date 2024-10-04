30823ede806acfaa041929803b18e82034befa71
OliwiaLidwin <oliwialidwin@outlook.com>
2024-10-04 11:25:15 -0400

    Added comments to carbon.py and golf.py for readability

--- hw2/carbon.py
+++ hw2/carbon.py
@@ -16,14 +16,14 @@ def euler(step_size, total_time): #Euler approximation method calculation values
     
     for i in range(1,int(total_time/step_size)+1): #works similarly to a recursive method.
         N[i] = N[i-1] - 0.00012*N[i-1]*step_size #decrease each amount of particles by the rate multiplied by the amount multiplied by the change in time.
-        
+        #N(t) = N_o - lambda*N_o*t
     return N
 
 def plot(width): #method was used to show figueres found in LaTex doc.
-    x1 = np.arange(0,20001,10)
+    x1 = np.arange(0,20001,10) #these define the time-steps
     x2 = np.arange(0,20001,100)
     x3 = np.arange(0,20001,1)
-    y1 = euler(10, 20000)
+    y1 = euler(10, 20000) #these use approximations
     y2 = euler(100, 20000)
     y3 = particleNum*np.exp(-0.00012*x3) #this is the exact method.
 
@@ -40,8 +40,12 @@ def plot(width): #method was used to show figueres found in LaTex doc.
     plt.show()
     plt.close()
     
+    #Plot of Figure 2 in Latex - same as Figure 1 but with a step width of 1000 for comparison
     x4 = np.arange(0,20001,width) #Width = 1000
     y4 = euler(width,20000)
+    print("Euler 2nd half life (11400 years ~12000 years): ", y4[12])
+    print("Exact 2nd half life (11400 years ~12000 years): ", y3[12000])
+
     plt.plot(x1, y1, label='Numerical (10-year intervals)', color='blue')
     plt.plot(x2, y2, label='Numerical (100-year intervals)', color='green')
     plt.plot(x3, y3, label='Exact Solution (yearly)', color='purple')
@@ -56,7 +60,7 @@ def plot(width): #method was used to show figueres found in LaTex doc.
     plt.show()
     plt.close()
 
-def plotByRequest(width): #Plotting only the 
+def plotByRequest(width): #Plotting only the requested graph
     x4 = np.arange(0,20001,width)
     y4 = euler(width,20000)
     #y1 = 0.00012*(particleNum*np.exp(-0.00012*x1))
@@ -72,21 +76,20 @@ def plotByRequest(width): #Plotting only the
     plt.close()
     
 
-def main():    
-    if len(sys.argv) != 2:
+def main():  
+    if len(sys.argv) == 1: #gets you Figures 1 and 2 from the Latex Doc
+        plot(1000) 
+        
+    elif len(sys.argv) == 2:
+        width_arg = sys.argv[1]
+        if width_arg.startswith('--plot='):
+            width = int(width_arg[len('--plot='):])
+            print(width)
+            plotByRequest(width)
+    else:
         print("Usage: python carbon.py --plot=<width>")
         sys.exit(1)
-        
-    width = 0   
-    
-# Uncomment the following line to create the figures found in the LaTex doc.
-    #     plot(1000) 
-    
-    width_arg = sys.argv[1]
-    if width_arg.startswith('--plot='):
-        width = int(width_arg[len('--plot='):])
-        print(width)
-        plotByRequest(width)
+            
 
 
 if __name__ == "__main__":
--- hw2/golf.py
+++ hw2/golf.py
@@ -10,124 +10,120 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-o1 = 45
-o2 = 30
-o3 = 15
-o4 = 9
-v = 70
-g = 9.8
-m = 0.046
-p = 1.29
-A = 0.0014
+v = 70 #initial velocity
+g = 9.8 #gravity
+m = 0.046 #mass
+p = 1.29 #air density
+A = 0.0014 #golf ball surface area
 time = 0.05 #time_steps
 
-
-def idealTraj(angle):
-    v_x = v*math.cos(angle)
+def idealTraj(angle): #only influence of gravity
+    v_x = v*math.cos(angle) #split initial velocity into x and y components
     v_y = v*math.sin(angle)
-    coordinates = [[0], [0]]
-    velocities = [[v_x], [v_y]]
-    index = 0
-    print(coordinates[1][index])
-    while coordinates[1][index] > 0 or coordinates[1][index] == 0:
+    coordinates = [[0], [0]] #starts at (0,0)
+    velocities = [[v_x], [v_y]] #starts with initial velocity components
+    index = 0 #this will be used to iterate through the list (could have alternatively used for each loop with a break when condition not met)
+    print(coordinates[1][index]) #make sure the plot starts at y = 0.
+    while coordinates[1][index] > 0 or coordinates[1][index] == 0: #while the y coordinate >=0
         # Changing X-coordinate
-        new_x = coordinates[0][index] + velocities[0][index] * time
+        new_x = coordinates[0][index] + velocities[0][index] * time #x = x_o + v_x(t)
         coordinates[0].append(new_x)
-        velocities[0].append(velocities[0][index])
+        velocities[0].append(velocities[0][index]) # v_x = v_ox
 
         # Changing Y-coordinate
-        new_y = coordinates[1][index] + velocities[1][index] * time
+        new_y = coordinates[1][index] + velocities[1][index] * time #y = y_o + v_y(t)
         coordinates[1].append(new_y)
-        velocity_y = velocities[1][index] - g * time
+        velocity_y = velocities[1][index] - g * time #v_y = v_oy - gt
         velocities[1].append(velocity_y)
 
-        index += 1
+        index += 1 #go to next coordinate
     print(f"Initial coordinates: x={coordinates[0][0]}, y={coordinates[1][0]}")
-    return coordinates
+    return coordinates #coordinates array is all the x, y positions
                 
 #Drag F_drag = -CpA(v^2); C is coefficient, p is 1.29 kg/m^3, A = 0.0014 m^2, v is velocity (magnitude)
 
 def dragTraj(angle):
     C = 0.5
-    v_x = v*math.cos(angle)
+    v_x = v*math.cos(angle) #Split v into it's components
     v_y = v*math.sin(angle)
-    coordinates = [[0], [0]]
+    coordinates = [[0], [0]] #initially at (0,0)
     velocities = [[v_x], [v_y]]
     index = 0
     print(coordinates[1][index])
-    while coordinates[1][index] > 0 or coordinates[1][index] == 0:
+    while coordinates[1][index] > 0 or coordinates[1][index] == 0: #while y >= 0
         # Changing X-coordinate
-        new_x = coordinates[0][index] + velocities[0][index] * time
+        new_x = coordinates[0][index] + velocities[0][index] * time #x = x_o + v_x(t)
         coordinates[0].append(new_x)
         velocity_x = velocities[0][index] - (1/m)* (C*p*A) * (velocities[0][index]**2 + velocities[1][index]**2)**0.5 * (velocities[0][index]) * time
-        velocities[0].append(velocity_x)
+        velocities[0].append(velocity_x) # v_x = v_ox - tCpA(v^2)/m
 
         # Changing Y-coordinate
-        new_y = coordinates[1][index] + velocities[1][index] * time
+        new_y = coordinates[1][index] + velocities[1][index] * time #y = y_oy + v_y(t)
         coordinates[1].append(new_y)
         velocity_y = velocities[1][index] - g * time - (1/m)* (C*p*A) * (velocities[0][index]**2 + velocities[1][index]**2)**0.5 * (velocities[1][index]) * time
-        velocities[1].append(velocity_y)
+        velocities[1].append(velocity_y) #v_y = v_oy - gt - tCpA(v^2)/m
         index += 1
+        
     print(f"Initial coordinates: x={coordinates[0][0]}, y={coordinates[1][0]}")
-    return coordinates
+    return coordinates #coordinates array is all the x, y positions
         
 def dimpledDragTraj(angle):
-    v_x = v*math.cos(angle)
+    v_x = v*math.cos(angle) #Split v into it's components
     v_y = v*math.sin(angle)
-    coordinates = [[0], [0]]
+    coordinates = [[0], [0]] #initially at (0,0)
     velocities = [[v_x], [v_y]]
     index = 0
     print(coordinates[1][index])
-    while coordinates[1][index] > 0 or coordinates[1][index] == 0:
-        if ((velocities[0][index]**2 + velocities[1][index]**2)**0.5) <= 14:
+    while coordinates[1][index] > 0 or coordinates[1][index] == 0: #while y >= 0
+        if ((velocities[0][index]**2 + velocities[1][index]**2)**0.5) <= 14: #Change C of drag accoring to velocity
             C = 0.5
         else:
             C = 7.0/((velocities[0][index]**2 + velocities[1][index]**2)**0.5)
         # Changing X-coordinate
-        new_x = coordinates[0][index] + velocities[0][index] * time
+        new_x = coordinates[0][index] + velocities[0][index] * time #x = x_o + v_x(t)
         coordinates[0].append(new_x)
         velocity_x = velocities[0][index] - (1/m)* (C*p*A) * (velocities[0][index]**2 + velocities[1][index]**2)**0.5 * (velocities[0][index]) * time
-        velocities[0].append(velocity_x)
+        velocities[0].append(velocity_x) # v_x = v_ox - tCpA(v^2)/m
 
         # Changing Y-coordinate
-        new_y = coordinates[1][index] + velocities[1][index] * time
+        new_y = coordinates[1][index] + velocities[1][index] * time #y = y_oy + v_y(t)
         coordinates[1].append(new_y)
         velocity_y = velocities[1][index] - g * time - (1/m)* (C*p*A) * (velocities[0][index]**2 + velocities[1][index]**2)**0.5 * (velocities[1][index]) * time
-        velocities[1].append(velocity_y)
+        velocities[1].append(velocity_y) #v_y = v_oy - gt - tCpA(v^2)/m
         index += 1
     print(f"Initial coordinates: x={coordinates[0][0]}, y={coordinates[1][0]}")
-    return coordinates
+    return coordinates #coordinates array is all the x, y positions
         
         
 def spunDimpledDraggedTraj(angle):
     Sowm = 0.25
-    v_x = v*math.cos(angle)
+    v_x = v*math.cos(angle) #Split v into it's components
     v_y = v*math.sin(angle)
-    coordinates = [[0], [0]]
+    coordinates = [[0], [0]]#initially at (0,0)
     velocities = [[v_x], [v_y]]
     index = 0
     print(coordinates[1][index])
-    while coordinates[1][index] > 0 or coordinates[1][index] == 0:
-        if ((velocities[0][index]**2 + velocities[1][index]**2)**0.5) <= 14:
+    while coordinates[1][index] > 0 or coordinates[1][index] == 0: #while y >= 0
+        if ((velocities[0][index]**2 + velocities[1][index]**2)**0.5) <= 14: #Change C of drag accoring to velocity
             C = 0.5
         else:
-            C = 7.0/((velocities[0][index]**2 + velocities[1][index]**2)**0.5)
+            C = 7.0/((velocities[0][index]**2 + velocities[1][index]**2)**0.5) 
         # Changing X-coordinate
-        new_x = coordinates[0][index] + velocities[0][index] * time
+        new_x = coordinates[0][index] + velocities[0][index] * time #x = x_o + v_x(t)
         coordinates[0].append(new_x)
         velocity_x = velocities[0][index] - (1/m)* (C*p*A) * (velocities[0][index]**2 + velocities[1][index]**2)**0.5 * (velocities[0][index]) * time - Sowm*velocities[1][index] * time
-        velocities[0].append(velocity_x)
+        velocities[0].append(velocity_x) # v_x = v_ox - tCpA(v^2)/m - Sowm *v_oy*t
 
         # Changing Y-coordinate
-        new_y = coordinates[1][index] + velocities[1][index] * time
+        new_y = coordinates[1][index] + velocities[1][index] * time #y = y_oy + v_y(t)
         coordinates[1].append(new_y)
         velocity_y = velocities[1][index] - g * time - (1/m)* (C*p*A) * (velocities[0][index]**2 + velocities[1][index]**2)**0.5 * (velocities[1][index]) * time + Sowm*velocities[0][index] * time
-        velocities[1].append(velocity_y)
+        velocities[1].append(velocity_y) #v_y = v_oy - gt - tCpA(v^2)/m - Sown *v_ox*t
         index += 1
     print(f"Initial coordinates: x={coordinates[0][0]}, y={coordinates[1][0]}")
-    return coordinates
+    return coordinates  #coordinates array is all the x, y positions
     
-def plotAllFourTraj(angle):
+def plotAllFourTraj(angle): #plot outputs from the methods above.
     angle_rad = math.radians(angle)
     ideal = idealTraj(angle_rad)
     drag = dragTraj(angle_rad)
@@ -145,20 +141,27 @@ def plotAllFourTraj(angle):
     plt.ylim(bottom=0)
     plt.grid()
     plt.legend()
-    plt.savefig(f"ProjectileAngle{angle}.png")
+    plt.savefig(f"ProjectileAngle{angle}.png") #save the figure 
     plt.show()
 
 
 def main():
-    if len(sys.argv) !=2:
-        print("Usage: python golf.py --plot=<theta>")
+    if (len(sys.argv) == 1): #if no optional parameter input, display default 4 plots used for LaTex file
+        plotAllFourTraj(45)
+        plotAllFourTraj(30)
+        plotAllFourTraj(15)
+        plotAllFourTraj(9)
+
+    elif (len(sys.argv) == 2): #if angle is provided, create it's plot.
+        theta_arg = sys.argv[1]
+        if theta_arg.startswith('--plot='):
+            theta = int(theta_arg[len('--plot='):])
+            print(theta)
+            plotAllFourTraj(theta)
+            
+    else:
+        print("Usage: python golf.py --plot=<theta>") #if wrong amount of parameters exit the code
         sys.exit(1)
-        
-    theta_arg = sys.argv[1]
-    if theta_arg.startswith('--plot='):
-        theta = int(theta_arg[len('--plot='):])
-        print(theta)
-        plotAllFourTraj(theta)
 
 
 
